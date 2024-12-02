efc6523c9c6745fc191983ed434b18c12b40fce4
Solaris-99 <e.cifuentes996@gmail.com>
2024-12-02 17:55:11 -0300

    attempt 1 of implementing path for type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,33 @@
 import java.util.*;
+import java.io.File;
 
 public class Main {
+    static final String PATH = System.getenv("PATH");
+
     public static void main(String[] args) throws Exception {
         List<String> builtins = new ArrayList<>();
-        builtins.add("echo");builtins.add("exit");builtins.add("type");
+        builtins.add("echo");
+        builtins.add("exit");
+        builtins.add("type");
 
-        while (true){
+        while (true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             String command;
             String commandArgs = "";
-            if(input.contains(" ")){
+            if (input.contains(" ")) {
                 int delim = input.indexOf(" ");
                 command = input.substring(0, delim);
-                commandArgs = input.substring(delim+1);
-            }else{
+                commandArgs = input.substring(delim + 1);
+            } else {
                 command = input;
             }
 
-
-            if(input.isBlank()){continue;}
-            else{
-                switch (command){
+            if (input.isBlank()) {
+                continue;
+            } else {
+                switch (command) {
                     case "echo":
                         System.out.println(commandArgs);
                         break;
@@ -31,20 +36,41 @@ public class Main {
                     case "type":
                         int ind = builtins.indexOf(commandArgs);
                         String out;
-                        if(ind > -1){
+                        if (ind > -1) {
                             out = commandArgs + " is a shell builtin";
-                        }else{
+                        }
+                        else if(getPath(commandArgs).isPresent()){
+                            out = commandArgs + " is "+ getPath(commandArgs).get();
+                        }
+                        else {
                             out = commandArgs + ": not found";
                         }
                         System.out.println(out);
                         break;
                     default:
                         System.out.println(input + ": command not found");
-
                 }
             }
-
-        }
-
+        }//loop end
     }
+
+    private static Optional<String> getPath(String command){
+        String[] pathDirs = PATH.split(":");
+        for (String path : pathDirs){
+            File dir = new File(path);
+            File[] files = dir.listFiles();
+            if (files != null) {
+                for(File file : files){
+                    if(file.canExecute()){
+                        String name = file.getName();
+                        if(name.equals(command)){
+                            return Optional.of(file.getAbsolutePath());
+                        }
+                    }
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
 }
