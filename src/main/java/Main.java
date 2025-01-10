79d5b88e02a37c52ec16560cda1e2a14ce281a22
Solaris-99 <e.cifuentes996@gmail.com>
2025-01-10 13:51:43 -0300

    test: file execution error output testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -65,7 +65,7 @@ public class Main {
                         //System.out.println(commandArr);
 
                         ProcessBuilder processBuilder = new ProcessBuilder(commandArgs.getTokens());
-                        processBuilder.redirectErrorStream(true);
+                        //processBuilder.redirectErrorStream(true);
                         Process process = processBuilder.start();
 
                         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
